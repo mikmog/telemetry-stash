3d5edde0d42ebcb05be9b2c05003e1e18d9740ff
Mikael Mogren <mikael.mogren@gmail.com>
2024-06-25 21:19:09 +0200

    SQL

---
+++ src/backend/Database/Ts.TelemetryDatabase.Sql/Stored Procedures/UpsertTelemetry.sql
@@ -0,0 +1,42 @@
+﻿-- =============================================
+-- Author:      Telemetry Stash
+-- Description: Upsert timestamp/telemetry.
+-- =============================================
+CREATE PROCEDURE[dbo].[UpsertTelemetry]
+(
+    @DeviceId SMALLINT,
+    @Timestamp DATETIMEOFFSET(4),
+	@Telemetry dbo.TelemetriesType READONLY
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @TsId INT;
+
+    SELECT @TsId = [Id]
+    FROM TimeStamps
+    WHERE DeviceId = @DeviceId AND Ts = @Timestamp;
+
+    IF @TsId IS NULL
+    BEGIN
+		-- Insert timestamp
+		DECLARE @Id TABLE(Id INT);
+        INSERT INTO Timestamps(DeviceId, Ts, Created)
+        OUTPUT inserted.Id INTO @Id
+        VALUES(@DeviceId, @Timestamp, GETUTCDATE());
+		SELECT @TsId = [Id] from @Id
+    END
+
+    -- Insert telemetry
+	INSERT INTO dbo.Telemetries(TimestampId, RegisterKeyId, [Value])
+	SELECT @TsId, RegisterKeyId, Value 
+	FROM @Telemetry AS T
+	WHERE NOT EXISTS (
+		SELECT 1 
+		FROM dbo.Telemetries 
+		WHERE TimestampId = @TsId AND RegisterKeyId = T.RegisterKeyId
+	);
+
+END
+GO
---
+++ src/backend/Database/Ts.TelemetryDatabase.Sql/User Defined Types/TelemetriesType.sql
@@ -0,0 +1,4 @@
+﻿CREATE TYPE [dbo].[TelemetriesType] AS TABLE(
+	[RegisterKeyId] [int] NOT NULL,
+	[Value] [decimal](19, 4) NOT NULL
+)
