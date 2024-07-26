6177e0d73dd30d3c3f6924b2fbe9cc1f0fcc01e3
Mikael Mogren <mikael.mogren@gmail.com>
2024-07-26 13:45:08 +0200

    Add Ts.TelemetryDatabase

---
+++ src/backend/Database/Ts.TelemetryDatabase/Repositories/RegisterRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace TelemetryStash.Database.Repositories;
+
+public interface IRegisterRepository
+{
+    Task<Register?> GetByTemplateAndSubset(int registerTemplateId, string subset, CancellationToken token = default);
+
+    Task<Register> Upsert(int registerTemplateId, string subset, CancellationToken token = default);
+}
+
+public class RegisterRepository(IDbProvider dbProvider) : IRegisterRepository
+{
+    public async Task<Register?> GetByTemplateAndSubset(int registerTemplateId, string subset, CancellationToken token = default)
+    {
+        return await dbProvider
+            .QuerySingle<Register>
+            (
+                where: register => register.RegisterTemplateId == registerTemplateId && register.Subset == subset,
+                cancellationToken: token
+            );
+    }
+
+    public async Task<Register> Upsert(int registerTemplateId, string subset, CancellationToken token = default)
+    {
+        return await dbProvider
+            .ExecuteSingle<Register>
+            (
+                storedProcedure: "dbo.GetOrCreateRegister",
+                parameters: new { RegisterTemplateId = registerTemplateId, Subset = subset },
+                cancellationToken: token
+            );
+    }
+}
---
+++ src/backend/Database/Ts.TelemetryDatabase/Repositories/RegisterSetRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace TelemetryStash.Database.Repositories;
+
+public interface IRegisterSetRepository
+{
+    Task<RegisterSet?> GetByDeviceAndIdentifier(int deviceId, string identifier, CancellationToken token = default);
+
+    Task<RegisterSet> Upsert(int deviceId, string identifier, CancellationToken token = default);
+}
+
+public class RegisterSetRepository(IDbProvider dbProvider) : IRegisterSetRepository
+{
+    public async Task<RegisterSet?> GetByDeviceAndIdentifier(int deviceId, string identifier, CancellationToken token = default)
+    {
+        return await dbProvider
+            .QuerySingle<RegisterSet>
+            (
+                where: set => set.DeviceId == deviceId && set.Identifier == identifier,
+                cancellationToken: token
+            );
+    }
+
+    public async Task<RegisterSet> Upsert(int deviceId, string identifier, CancellationToken token = default)
+    {
+        return await dbProvider
+            .ExecuteSingle<RegisterSet>
+            (
+                storedProcedure: "dbo.GetOrCreateRegisterSet",
+                parameters: new { DeviceId = deviceId, Identifier = identifier },
+                cancellationToken: token
+            );
+    }
+}
