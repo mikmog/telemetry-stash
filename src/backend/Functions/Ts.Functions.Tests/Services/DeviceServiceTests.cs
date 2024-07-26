a50f6c34f633fc8d9efe4701fee8cf792a1ea849
Mikael Mogren <mikael.mogren@gmail.com>
2024-07-26 19:35:04 +0200

    Add Ts.Functions.Tests (#11)

---
+++ src/backend/Functions/Ts.Functions.Tests/Services/DeviceServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using NSubstitute;
+using TelemetryStash.Database;
+using TelemetryStash.Database.Repositories;
+using TelemetryStash.Functions.Services;
+
+namespace TelemetryStash.Functions.Tests.Services;
+
+public class DeviceServiceTests
+{
+    [Fact]
+    public async Task DeviceService_GetOrCreate_adds_Device_to_database()
+    {
+        // Arrange
+        using var cacheProvider = new CacheProvider();
+        var repository = Substitute.For<IDeviceRepository>();
+
+        var sut = new DeviceService(repository, cacheProvider.HybridCache);
+
+        // Act
+        await sut.GetOrCreate("ESP32");
+
+        // Assert
+        await repository.Received(1).Upsert(Arg.Is("ESP32"), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task DeviceService_GetOrCreate_caches_Device()
+    {
+        // Arrange
+        using var cacheProvider = new CacheProvider();
+        var repository = Substitute.For<IDeviceRepository>();
+
+        var sut = new DeviceService(repository, cacheProvider.HybridCache);
+
+        // Act
+        await sut.GetOrCreate("ESP32");
+        await sut.GetOrCreate("ESP32");
+
+        // Assert
+        await repository.Received(1).Upsert("ESP32", Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task DeviceService_GetOrCreate_blocks_first_call_to_database_until_finished()
+    {
+        // Arrange
+        using var cacheProvider = new CacheProvider();
+        var repository = Substitute.For<IDeviceRepository>();
+
+        /// Simulate slow first database call
+        repository
+            .Upsert(Arg.Is("ESP32"), Arg.Any<CancellationToken>())!
+            .Returns(Task.Delay(100).ContinueWith(_ => new Device(1, "ESP32")));
+
+        var sut = new DeviceService(repository, cacheProvider.HybridCache);
+        var tasks = new List<Task>();
+
+        // Act
+        for (var i = 0; i < 10; i++)
+        {
+            tasks.Add(sut.GetOrCreate("ESP32"));
+        }
+        await Task.WhenAll(tasks);
+
+        // Assert
+        Assert.DoesNotContain(tasks, t => t.IsFaulted);
+        await repository.Received(1).Upsert(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+}
---
+++ src/backend/Functions/Ts.Functions.Tests/Services/TelemetryServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using NSubstitute;
+using System.Text.Json;
+using TelemetryStash.Database;
+using TelemetryStash.Database.Repositories;
+using TelemetryStash.Functions.Services;
+using TelemetryStash.Functions.TelemetryTrigger;
+
+namespace TelemetryStash.Functions.Tests.Services;
+
+public class TelemetryServiceTests
+{
+    [Fact]
+    public async Task TelemetryService_adds_Telemetry_to_database()
+    {
+        // Arrange
+        using var cacheProvider = new CacheProvider();
+        var telemetryRepository = Substitute.For<ITelemetryRepository>();
+
+        var deviceService = Substitute.For<IDeviceService>();
+        deviceService.GetOrCreate(Arg.Any<string>()).Returns(new Device(1, "TestDevice"));
+
+        var registerSetService = Substitute.For<IRegisterSetService>();
+        registerSetService.GetOrCreate(Arg.Any<int>(), Arg.Any<string>()).Returns(new RegisterSet(1, 1, "PowerMeter"));
+
+        var registerTemplateService = Substitute.For<IRegisterTemplateService>();
+        registerTemplateService.GetOrCreate(Arg.Any<int>(), Arg.Any<string>()).Returns(new RegisterTemplate(1, 1, "L1Current"));
+
+        var registerService = Substitute.For<IRegisterService>();
+        registerService.GetOrCreate(Arg.Any<int>(), Arg.Any<string>()).Returns(new Register(1, 1, "L1Current"));
+
+        var sut = new TelemetryService(telemetryRepository, deviceService, registerSetService, registerTemplateService, registerService);
+
+        // Act
+        await sut.Process("TestDevice", CreateTelemetry());
+
+        // Assert
+        await telemetryRepository.Received(1).Upsert(
+            Arg.Is(1),
+            Arg.Any<DateTimeOffset>(),
+            Arg.Is<List<(int RegisterKey, decimal Value)>>(a => a.Count == 5));
+    }
+
+    private static TelemetryRequest CreateTelemetry()
+    {
+        var json = """
+        {
+            "ts": "2309012013361234567",
+            "reg": {
+                "PowerMeter": {
+                    "C1": 5,
+                    "C2": 6,
+                    "C3": 7.01
+                },
+                "Am2320": {
+                    "Hum": 80,
+                    "Temp": 21.44
+                }
+            }
+        }
+        """;
+
+        return JsonSerializer.Deserialize<TelemetryRequest>(json)!;
+    }
+}
