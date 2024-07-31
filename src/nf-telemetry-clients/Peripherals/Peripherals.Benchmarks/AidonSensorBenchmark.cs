336e75a6898281327c0269ea0e001f630a81ee5e
Mikael Mogren <mikael.mogren@gmail.com>
2024-07-31 14:44:09 +0200

    nf: Upgrade nugets

---
+++ src/nf-telemetry-clients/Peripherals/Peripherals.Benchmarks/AidonSensorBenchmark.cs
@@ -0,0 +1,33 @@
+﻿using nanoFramework.Benchmark;
+using nanoFramework.Benchmark.Attributes;
+using nanoFramework.Hardware.Esp32;
+using TelemetryStash.Aidon.Sensor;
+
+namespace TelemetryStash.Peripherals.Benchmarks
+{
+    [ConsoleParser]
+    [IterationCount(1)]
+    public class AidonSensorBenchmark
+    {
+        private AidonSensor _aidonSensor;
+
+        [Setup]
+        public void Setup()
+        {
+            var settings = new AidonSensorSettings
+            {
+                ComPort = "COM2",
+                RegisterSetIdentifier = "Test",
+                RxPin = 20,
+                RxComPort = DeviceFunction.COM2_RX
+            };
+            _aidonSensor = new AidonSensor(settings);
+        }
+
+        [Benchmark, Baseline]
+        public void CreateSerialPort()
+        {
+            _aidonSensor.Open();
+        }
+    }
+}
