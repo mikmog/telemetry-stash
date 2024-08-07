37690a977bf05d7dab6dbbabe90b4778f7031cd0
Mikael Mogren <mikael.mogren@gmail.com>
2024-08-07 22:13:14 +0200

    Refactor UpsertTelemetry

--- src/backend/Database/Ts.TelemetryDatabase.Sql/Stored Procedures/Telemetry/UpsertTelemetry.sql
+++ src/backend/Database/Ts.TelemetryDatabase.Sql/Stored Procedures/Telemetry/UpsertTelemetry.sql
@@ -1,6 +1,5 @@
 ﻿-- =============================================
--- Description: Upsert timestamp/telemetry.
--- TODO: Refactor
+-- Description: AddOrGet timestamp and upsert telemetry.
 -- =============================================
 CREATE PROCEDURE dbo.UpsertTelemetry
 (
@@ -13,68 +12,49 @@ BEGIN
     SET NOCOUNT ON;
     SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
-    BEGIN TRY  
-        DECLARE @TimestampId INT;
+    DECLARE @TimestampId INT;
 
-        SELECT TOP 1
-            @TimestampId = Id
-        FROM
-            dbo.Timestamps
-        WHERE
-            DeviceId = @DeviceId
-            AND ClientTimestamp = @ClientTimestamp;
+    -- Insert timestamp. Happy path
+    -- If timestamp already exists, get the Id
+    BEGIN TRY
 
-        IF @TimestampId IS NULL
-        BEGIN
-		    -- Insert timestamp
-            INSERT INTO
-                dbo.Timestamps
-                (
-                    DeviceId
-                    ,ClientTimestamp
-                    ,Created
-                )
-            VALUES
-            (
-                @DeviceId
-                ,@ClientTimestamp
-                ,GETUTCDATE()
-            );
-
-		    SET @TimestampId = SCOPE_IDENTITY() 
-        END
-
-        -- Upsert telemetry
-        MERGE dbo.Telemetries AS Target
-        USING @Telemetry AS Source
-        ON (Target.TimestampId = @TimestampId AND Target.RegisterId = Source.RegisterId)
-
-        WHEN NOT MATCHED BY Target THEN
-        INSERT (RegisterId, TimestampId ,Value)
+        INSERT INTO
+            dbo.Timestamps(DeviceId ,ClientTimestamp ,Created)
         VALUES
-        (
-            Source.RegisterId
-            ,@TimestampId
-            ,Source.Value
-        )
+            (@DeviceId ,@ClientTimestamp ,GETUTCDATE());
 
-        WHEN MATCHED THEN UPDATE SET
-            Target.Value = Source.Value
-            ;
+		SET @TimestampId = SCOPE_IDENTITY()
 
     END TRY
     BEGIN CATCH
-        DECLARE @ErrorMessage NVARCHAR(MAX);
-        DECLARE @ErrorSeverity INT;
-        DECLARE @ErrorState INT;
- 
-        SELECT
-            @ErrorMessage = ERROR_MESSAGE(),
-            @ErrorSeverity = ERROR_SEVERITY(),
-            @ErrorState = ERROR_STATE();
- 
-        RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+        IF (ERROR_NUMBER() = 2601) -- Duplicate key constraint
+            SELECT
+                @TimestampId = Id
+            FROM
+                dbo.Timestamps
+            WHERE
+                DeviceId = @DeviceId AND ClientTimestamp = @ClientTimestamp;
+        ELSE
+           THROW;
+
     END CATCH;
+        
+    -- Upsert telemetry
+    MERGE dbo.Telemetries AS Target
+    USING @Telemetry AS Source
+    ON (Target.TimestampId = @TimestampId AND Target.RegisterId = Source.RegisterId)
+
+    WHEN NOT MATCHED BY Target THEN
+    INSERT (RegisterId, TimestampId ,Value)
+    VALUES
+    (
+        Source.RegisterId
+        ,@TimestampId
+        ,Source.Value
+    )
+
+    WHEN MATCHED THEN UPDATE SET
+        Target.Value = Source.Value;
 
 END
 GO
