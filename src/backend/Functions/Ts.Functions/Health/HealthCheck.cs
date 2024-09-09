05689f9d0e47a2f3aa702993826194c868bbb8c5
Mikael Mogren <mikael.mogren@gmail.com>
2024-09-09 07:53:38 +0200

    Revert

--- src/backend/Functions/Ts.Functions/Health/HealthCheck.cs
+++ src/backend/Functions/Ts.Functions/Health/HealthCheck.cs
@@ -1,22 +1,22 @@
-﻿//using Microsoft.Azure.Functions.Worker;
-//using Microsoft.Azure.Functions.Worker.Http;
-//using Microsoft.Extensions.Diagnostics.HealthChecks;
-//using System.Net;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Net;
 
-//namespace TelemetryStash.Functions.Health;
+namespace TelemetryStash.Functions.Health;
 
-//public class HealthCheck(HealthCheckService healthCheckService)
-//{
-//    [Function("HealthCheck")]
-//    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequestData req, FunctionContext executionContext)
-//    {
-//        var status = await healthCheckService.CheckHealthAsync();
+public class HealthCheck(HealthCheckService healthCheckService)
+{
+    [Function("HealthCheck")]
+    public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequestData req, FunctionContext executionContext)
+    {
+        var status = await healthCheckService.CheckHealthAsync();
 
-//        var response = req.CreateResponse();
-//        response.StatusCode = status.Status == HealthStatus.Healthy ? HttpStatusCode.OK : HttpStatusCode.FailedDependency;
-//        await response.WriteAsJsonAsync(new Response(Enum.GetName(status.Status), status.TotalDuration.TotalMilliseconds));
-//        return response;
-//    }
+        var response = req.CreateResponse();
+        response.StatusCode = status.Status == HealthStatus.Healthy ? HttpStatusCode.OK : HttpStatusCode.FailedDependency;
+        await response.WriteAsJsonAsync(new Response(Enum.GetName(status.Status), status.TotalDuration.TotalMilliseconds));
+        return response;
+    }
 
-//    private record Response(string? Status, double Duration);
-//}
+    private record Response(string? Status, double Duration);
+}
