9841c60b796cf4e9abbcdd9b5801f9c05cb08d37
Mikael Mogren <mikael.mogren@gmail.com>
2025-02-28 22:30:00 +0100

    Implement hall sensor

--- src/nf-telemetry-clients/Clients/RipTide.Nfirmware/Program.cs
+++ src/nf-telemetry-clients/Clients/RipTide.Nfirmware/Program.cs
@@ -1,14 +1,6 @@
-using NeoPixel.Peripheral;
-using System;
 using System.Diagnostics;
 using System.Threading;
 using TelemetryStash.AdcSensor;
-using TelemetryStash.BuzzerPeripheral;
-using TelemetryStash.Ds18b20Sensor;
-using TelemetryStash.IliDisplay;
-using TelemetryStash.IO.Peripherals;
-using TelemetryStash.MpuxxxxGyro.Sensor;
-using TelemetryStash.Peripherals.SdCard;
 
 
 namespace RipTide.Nfirmware
@@ -19,44 +11,21 @@ namespace RipTide.Nfirmware
         {
             Thread.Sleep(2000);
 
-            //var buttonPressed = false;
+            var ss49e = new Ss49eHallSensor(new int[] { 0, 1 }, adcReadScale: 45, true);
+            ss49e.CalibrateAdcChannelOffsets();
+            while (true)
+            {
+                var value = ss49e.Read();
+                if (value == -1)
+                {
+                    ss49e.CalibrateAdcChannelOffsets();
+                    continue;
+                }
 
-            //var button = new Button(8);
-            //button.OnButtonDown(() =>  buttonPressed = true); 
+                Debug.WriteLine($"Value: {value}");
+            }
 
-            //if (buttonPressed)
-            //{
-            //    Debug.WriteLine("Button pressed. Sleeping one minute");
-            //    Thread.Sleep(TimeSpan.FromMinutes(1));
-            //}
-
-            //var ds18b20 = new Ds18b20Sensor();
-            //ds18b20.RunDemo();
-
-            //var neo = new NeoPixelGauge(pixelsCount: 45, new[] { Color.Green, Color.Yellow, Color.Red }, pin: 11);
-            //neo.DemoRun();
-
-            //var ss49e = new Ss49eHallSensor();
-            //ss49e.RunDemo();
-
-            //var xdb401 = new Xdb401PressureSensor();
-            //xdb401.RunDemo();
-
-            //var buzzer = new PiezoBuzzer();
-            //buzzer.RunDemo();
-
-            //var pwm = new Pwm();
-            //pwm.RunDemo();
-
-            //var gyro = new Mpu6050Gyro();
-            //gyro.RunDemo();
-
-            //var display = new Ili9341Display();
-            //display.RunDemo();
-
-            var microSd = new MicroSdCard();
-            microSd.RunDemo();
-
+            Debug.WriteLine("Zzzz");
             Thread.Sleep(Timeout.Infinite);
         }
     }
