4b45dfa299e034ad7753841f7b0fb6c0e2da194a
Mikael Mogren <mikael.mogren@gmail.com>
2025-03-02 19:19:05 +0100

    Implement neo pixel (#55)
    * Implement neo pixel
    
    * Update src/nf-telemetry-clients/Peripherals/NeoPixel.Peripheral/NeoPixelGauge.cs
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/nf-telemetry-clients/Peripherals/NeoPixel.Peripheral/NeoPixelGauge.cs
+++ src/nf-telemetry-clients/Peripherals/NeoPixel.Peripheral/NeoPixelGauge.cs
@@ -1,12 +1,10 @@
 ﻿using CCSWE.nanoFramework.NeoPixel;
 using CCSWE.nanoFramework.NeoPixel.Drivers;
+using System;
 using System.Drawing;
 using System.Threading;
 
 // https://docid81hrs3j1.cloudfront.net/medialibrary/2018/10/WS2812B_V1.4_EN_18090714224701.pdf
-// Enable LDO2 on UM FeatherS3
-//var gpioController = new GpioController();
-//gpioController.OpenPin(39, PinMode.Output).Write(PinValue.High);
 
 namespace NeoPixel.Peripheral
 {
@@ -14,9 +12,14 @@ namespace NeoPixel.Peripheral
     {
         private readonly Color[] _colors;
         private readonly NeoPixelStrip _pixels;
+        private readonly Thread _gaugeThread;
+        
+        private volatile int _currentPosition;
+        private volatile int _requestedPosition;
 
         public NeoPixelGauge(ushort pixelsCount, Color[] gaugeColors, byte pin)
         {
+            // TODO: Decrease pulse transfer times
             _pixels = new NeoPixelStrip(pin, pixelsCount, new Ws2812B());
             _colors = new Color[pixelsCount];
 
@@ -40,25 +43,98 @@ namespace NeoPixel.Peripheral
                     _colors[index++] = color;
                 }
             }
+
+            _gaugeThread = new Thread(SetPosition);
         }
 
-        public void DemoRun()
+        public void Initialize()
         {
-            var delay = 500;
+            _gaugeThread.Start();
+            for (var i = 0; i < _pixels.Count; i++)
+            {
+                _pixels.SetLed(i, _colors[i]);
+                _pixels.Update();
+                Thread.Sleep(1);
+            }
+            
+            for (var i = 0; i < _pixels.Count; i++)
+            {
+                _pixels.SetLed(i, Color.Black);
+                _pixels.Update();
+                Thread.Sleep(1);
+            }
+        }
 
+        public void SetPosition(int position)
+        {
+            _requestedPosition = Math.Min(position, _colors.Length-1);
+        }
+
+        private void SetPosition()
+        {
             while (true)
             {
-                _pixels.Clear();
-                _pixels.Update();
-
-                for (var i = 0; i < _pixels.Count; i++)
+                if (_currentPosition == _requestedPosition)
                 {
-                    _pixels.SetLed(i, _colors[i]);
-                    _pixels.Update();
-                    Thread.Sleep(10);
+                    Thread.Sleep(1);
+                    continue;
                 }
 
-                Thread.Sleep(delay * 5);
+                var currentPosition = _currentPosition;
+                var requestedPosition = _requestedPosition;
+
+                var goingUp = currentPosition < requestedPosition;
+                if (goingUp)
+                {
+                    while (currentPosition < requestedPosition)
+                    {
+                        // Increase the speed of the gauge if the requested position is far away
+                        var increments = (requestedPosition - currentPosition) switch
+                        {
+                            > 10 => 3,
+                            > 5 => 2,
+                            _ => 1,
+                        };
+
+                        _pixels.SetLed(++currentPosition, _colors[currentPosition]);
+                        if (currentPosition % increments == 0 || currentPosition >= requestedPosition || requestedPosition != _requestedPosition)
+                        {
+                            _pixels.Update();
+                            Thread.Sleep(0);
+                        }
+                        if (requestedPosition != _requestedPosition)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                // going down
+                else
+                {
+                    while (currentPosition > requestedPosition)
+                    {
+                        var increments = (currentPosition - requestedPosition) switch
+                        {
+                            > 5 => 2,
+                            _ => 1,
+                        };
+
+                        _pixels.SetLed(currentPosition--, Color.Black);
+                        if (currentPosition % increments == 0 || requestedPosition == currentPosition || requestedPosition != _requestedPosition)
+                        { 
+                            _pixels.Update();
+                            Thread.Sleep(0);
+                        }
+
+                        if (requestedPosition != _requestedPosition)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                _currentPosition = currentPosition;
             }
         }
     }
