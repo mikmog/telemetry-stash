2374d2ac1f7a87a0590b4ca87475437f2f4b7efb
Mikael Mogren <mikael.mogren@gmail.com>
2025-07-11 20:35:26 +0200

    bms

---
+++ src/nf-telemetry-clients/Peripherals/Bms.Peripheral/Daly/ActiveBalanceBms.cs
@@ -0,0 +1,35 @@
+﻿using Iot.Device.Modbus.Client;
+using nanoFramework.Hardware.Esp32;
+
+namespace TelemetryStash.Peripherals.Bms.Daly
+{
+    // Uses a MAX485 Modbus Module
+    // 
+    public class ActiveBalanceBms
+    {
+        const byte DeviceAddress = 0xD2;
+
+        private ModbusClient _modbusClient;
+
+        public void Initialize()
+        {
+            Configuration.SetPinFunction(0, DeviceFunction.COM2_RTS);
+            Configuration.SetPinFunction(43, DeviceFunction.COM2_TX);
+            Configuration.SetPinFunction(44, DeviceFunction.COM2_RX);
+
+            _modbusClient = new ModbusClient("COM2");
+            _modbusClient.ReadTimeout = _modbusClient.WriteTimeout = 2000;
+        }
+
+        public double RadValue(Register register)
+        {
+            var data = _modbusClient.ReadHoldingRegisters(DeviceAddress, register.Address, count: 1);
+            if (data == null || data.Length == 0)
+            {
+                return double.NaN;
+            }
+
+            return register.Value(data[0]);
+        }
+    }
+}
---
+++ src/nf-telemetry-clients/Peripherals/Bms.Peripheral/Daly/Registers.cs
@@ -0,0 +1,36 @@
+﻿namespace TelemetryStash.Peripherals.Bms.Daly
+{
+    public static class Registers
+    {
+#pragma warning disable format
+
+        ///                                                  Address  SI-Factor Offset
+        public static readonly Register TotalVoltage =      new(0x28, 10,       0);       // 35.0v
+        public static readonly Register TotalCurrent =      new(0x29, 10,       -30000);  // 10.00A
+        public static readonly Register RemainingCapacity = new(0x30, 10,       0);       // 0.1Ah
+        public static readonly Register PowerInWatts =      new(0x39, 1,        0);       // 50W
+        public static readonly Register Temperature1 =      new(0x20, 1,        -40);     // 25.0C
+        public static readonly Register Temperature2 =      new(0x21, 1,        -40);     // 25.0C
+
+#pragma warning restore format
+
+        public static double Value(this Register register, short value)
+        {
+            return (value + register.Offset) / (double)register.ConversionFactor;
+        }
+    }
+
+    public readonly struct Register
+    {
+        public Register(byte address, short conversionFactor, short offset)
+        {
+            Address = address;
+            ConversionFactor = conversionFactor;
+            Offset = offset;
+        }
+
+        public byte Address { get; }
+        public short ConversionFactor { get; }
+        public short Offset { get; }
+    }
+}
